dff092f8409a022cfa18732d86036443b490a436
pepo camus <jlcamus@miuandes.cl>
2021-03-24 18:15:27 -0300

    lab_1

--- lab_1.py
+++ lab_1.py
@@ -8,6 +8,22 @@ Created on Tue Mar 23 16:10:32 2021
 import random as rnd
 import numpy as np
 
+def division(L):
+    if len(L)<=10:
+        return L
+    else:
+        L1=[]
+        L=np.array_split(L,len(L)/10 )
+        
+        for i in L:
+            L1.append(list(i))
+
+        return L1
+
+
+
+
+
 cartas=int(input('Cantidad de cartas: '))
 
 p1=0
@@ -18,12 +34,75 @@ L=L*2
 rnd.shuffle(L)
 
 oculto=['*']*len(L)
-oculto=np.array_split(oculto,len(oculto)/10 )
-oculto=np.array(oculto)
+oculto=division(oculto)
 
-Tablero=np.array_split(L,len(L)/10 )
-Tablero=np.array(Tablero)
+Tablero=L
+Tablero=division(Tablero)
 
 for i in oculto:
-    print(*i)
+        print(*i)
+    
+while cartas>0:
+    print('Turno p1')
+    fila=int(input('Seleccione una fila: '))-1
+    columna=int(input('Seleccione una columna: '))-1
+    
+    oculto[fila][columna]=Tablero[fila][columna]
+    
+    for i in oculto:
+        print(*i)
+        
+    fila2=int(input('Seleccione una fila: '))-1
+    columna2=int(input('Seleccione una columna: '))-1
+    
+    oculto[fila2][columna2]=Tablero[fila2][columna2]
+    
+    for i in oculto:
+        print(*i)
+        
+    if Tablero[fila][columna]==Tablero[fila2][columna2]:
+        p1+=1
+        cartas-=1
+        
+        oculto[fila][columna]=' '
+        oculto[fila2][columna2]=' '
+        
+    else:
+        oculto[fila][columna]='*'
+        oculto[fila2][columna2]='*'
+        
+    if cartas>0:
+        print('Turno p2')
+        fila=int(input('Seleccione una fila: '))-1
+        columna=int(input('Seleccione una columna: '))-1
+    
+        oculto[fila][columna]=Tablero[fila][columna]
+    
+        for i in oculto:
+            print(*i)
+        
+        fila2=int(input('Seleccione una fila: '))-1
+        columna2=int(input('Seleccione una columna: '))-1
+    
+        oculto[fila2][columna2]=Tablero[fila2][columna2]
+    
+        for i in oculto:
+            print(*i)
+        
+        if Tablero[fila][columna]==Tablero[fila2][columna2]:
+            p2+=1
+            cartas-=1
+        
+            oculto[fila][columna]=' '
+            oculto[fila2][columna2]=' '
+            
+        else:
+            oculto[fila][columna]='*'
+            oculto[fila2][columna2]='*'
+    
+    
+    
+    
+    
+    
     
